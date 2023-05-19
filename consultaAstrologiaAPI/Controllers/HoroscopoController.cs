ef8bf60ced470f6de96fce10965a7ecb653951e1
jojosuelobo <josuelobo2000@gmail.com>
2023-05-18 21:18:20 -0300

    primeiro commit

---
+++ consultaAstrologiaAPI/Controllers/HoroscopoController.cs
@@ -0,0 +1,19 @@
+﻿using consultaAstrologiaAPI.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace consultaAstrologiaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HoroscopoController : Controller
+    {
+        [HttpGet(Name = "GetHoroscopoDTO")]
+        public HoroscopoDTO Get()
+        {
+            HoroscopoDTO horoscopo = new HoroscopoDTO();
+            return horoscopo;
+        }
+
+        // TESTE
+    }
+}
---
+++ consultaAstrologiaAPI/DTO/HoroscopoDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace consultaAstrologiaAPI.DTO
+{
+    public class HoroscopoDTO
+    {
+        public string signo { get; set; }
+
+        private static readonly string[] Signos = {
+        "Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem",
+        "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"
+        };
+
+        public HoroscopoDTO()
+        {
+            Random random = new Random();
+            int index = random.Next(0, Signos.Length);
+            signo = Signos[index];
+        }
+
+    }
+}
